cef4c672ad4aca56560573bef83cca8870c396e5
Andrii Kunitsyn <akunitsyn@onwardtek.com>
2026-01-15 16:38:52 +0100

    naming

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9a412abfa177a747c9b15c795f405fdc5e436c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8772dcee4454c2428b1b88a3b771961ea8fc8dbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
