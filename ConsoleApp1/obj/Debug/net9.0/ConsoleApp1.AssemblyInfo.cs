b4b39882884f37a32d969c9f3b85db0371fdacb0
Andrii Kunitsyn <akunitsyn@onwardtek.com>
2026-01-21 09:46:35 +0100

    pdf

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a467304397028a5360f06f739692cb93a86137")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf3b2faa1847d413cb75ca91c7b7ead5fc1bd8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
